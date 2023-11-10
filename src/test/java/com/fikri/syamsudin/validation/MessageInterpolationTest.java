336f7eb5dc4327b2348b3af73cd30ddbf37a079c
fikri <fuadfikri01@gmail.com>
2023-11-11 05:16:56 +0700

    message internazionalization

--- src/test/java/com/fikri/syamsudin/validation/MessageInterpolationTest.java
+++ src/test/java/com/fikri/syamsudin/validation/MessageInterpolationTest.java
@@ -5,10 +5,26 @@ import com.fikri.syamsudin.validation.model.Payment;
 import com.fikri.syamsudin.validation.model.group.VirtualAccountPaymentGroup;
 import org.junit.jupiter.api.Test;
 
+import java.util.Locale;
+
 public class MessageInterpolationTest extends AbstractTest{
 
     @Test
     void paymentTest() {
+        Payment payment = new Payment();
+        payment.setOrderId("");
+        payment.setAmount(1000L);
+        payment.setVirtualAccount("VA123");
+
+        Customer customer = new Customer();
+        payment.setCustomer(customer);
+
+        this.validate(payment);
+    }
+
+    @Test
+    void internalizationTest() {
+        Locale.setDefault(new Locale("in","ID"));
         Payment payment = new Payment();
         payment.setOrderId("");
         payment.setAmount(1000L);
